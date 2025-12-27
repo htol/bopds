adbfff6b4cb3bb0d7dd381b3c6d6bc551c32f0be
htol <github@h-tol.net>
2025-12-27 02:30:23 +0100

    improving error handling to prevent server crash

--- app/bopds.go
+++ app/bopds.go
@@ -13,6 +13,12 @@ import (
 
 var storage *repo.Repo
 
+// respondWithError logs an error and sends an HTTP error response
+func respondWithError(w http.ResponseWriter, message string, err error, statusCode int) {
+	log.Printf("%s: %v", message, err)
+	http.Error(w, message, statusCode)
+}
+
 func CLI(args []string) int {
 	var app appEnv
 	if err := app.fromArgs(args); err != nil {
@@ -100,7 +106,8 @@ func indexHandler() http.Handler {
 func getAuthors(w http.ResponseWriter, r *http.Request) {
 	authors, err := storage.GetAuthors()
 	if err != nil {
-		log.Fatal("getAuthor: ", err)
+		respondWithError(w, "Failed to get authors", err, http.StatusInternalServerError)
+		return
 	}
 	for _, author := range authors {
 		fmt.Fprintf(w, "%d: %s, %s, %s\n", storage.AuthorsCache[author], author.FirstName, author.MiddleName, author.LastName)
@@ -116,12 +123,13 @@ func getAuthorsByLetter() http.Handler {
 		}
 		authors, err := storage.GetAuthorsByLetter(letters)
 		if err != nil {
-			log.Fatal("getAuthorsByLetter: ", err)
+			respondWithError(w, "Failed to get authors by letter", err, http.StatusInternalServerError)
+			return
 		}
 		/* for _, author := range authors {
 			fmt.Fprintf(w, "%d: %s, %s, %s\n", storage.AuthorsCache[author], author.FirstName, author.MiddleName, author.LastName)
 		} */
-		w.Header().Set("Content-Type", "applcation/json")
+		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(authors)
 	}
 	return http.HandlerFunc(hf)
@@ -130,7 +138,8 @@ func getAuthorsByLetter() http.Handler {
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	books, err := storage.GetBooks()
 	if err != nil {
-		log.Fatal("getBooks: ", err)
+		respondWithError(w, "Failed to get books", err, http.StatusInternalServerError)
+		return
 	}
 
 	for _, book := range books {
@@ -147,9 +156,10 @@ func getBooksByLetter() http.Handler {
 		}
 		books, err := storage.GetBooksByLetter(letters)
 		if err != nil {
-			log.Fatal("getBooksByLetter: ", err)
+			respondWithError(w, "Failed to get books by letter", err, http.StatusInternalServerError)
+			return
 		}
-		w.Header().Set("Content-Type", "applcation/json")
+		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(books)
 	}
 	return http.HandlerFunc(hf)
